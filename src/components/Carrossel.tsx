d9d2795936c9ac08298c05eddab443dadbf9d989
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2023-08-17 16:50:16 -0300

    carrossel em construção

--- src/components/Carrossel.tsx
+++ src/components/Carrossel.tsx
@@ -1,4 +1,8 @@
+import React from "react";
 import Carousel from "nuka-carousel";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 
 interface Game {
     id: number,
@@ -14,21 +18,45 @@ interface CarrosselProps {
 export default function Carrossel({ games }: CarrosselProps) {
 
     function pickGameId(game: Game) {
-        console.log('GAME ID:', game.id)
+        console.log('GAME ID:', game.id);
     }
 
+    const responsiveSlidesToShow = {
+        small: 2,
+        medium: 3,
+        large: 4
+    };
+
+    const initialSlideIndex = Math.floor(games.length / 2); // Calcula o índice do slide central
+
     return (
-        <>
-            <Carousel cellAlign="left" slidesToShow={4}>
-                {games.map((game: any) => (
+        <Carousel
+            
+            cellAlign="center"
+            cellSpacing={20}
+            autoplay={true}
+            wrapAround={true}
+            renderBottomCenterControls={false}
+            slidesToShow={responsiveSlidesToShow.medium}
+            
+            slideIndex={initialSlideIndex} // Define o índice inicial para o slide central
+            renderCenterLeftControls={({ previousSlide }) => (
+                <button onClick={previousSlide}><ArrowBackIosIcon sx={{fontSize: 35}}/></button>
+            )}
+            renderCenterRightControls={({ nextSlide }) => (
+                <button onClick={nextSlide}><ArrowForwardIosIcon sx={{fontSize: 35}}/></button>
+            )}
+        >
+            {games.map((game: Game) => (
+                <div className="carousel-card" key={game.id}>
                     <img
-                        key={game.id}
                         src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`}
-                        alt={game.id}
+                        alt={game.id.toString()}
                         onClick={() => pickGameId(game)}
+                        className="cursor-pointer"
                     />
-                ))}
-            </Carousel>
-        </>
-    )
+                </div>
+            ))}
+        </Carousel>
+    );
 }
